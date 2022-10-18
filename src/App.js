7cef285c5bd01610a1868616872ba83ff6fc269c
JUAN-BH <juanmita_bh@hotmail.com>
2022-10-18 16:13:35 -0400

    Mejorando la organizacion, render props, HOCs y composicion de componentes

--- src/App.js
+++ src/App.js
@@ -1,34 +1,96 @@
 import React, { useState } from "react";
+import { ToDoHeader } from "./components/ToDoHeader";
 import { ToDoCounter } from "./components/ToDoCounter";
 import { ToDoList } from "./components/ToDoList";
 import { ToDoItem } from "./components/ToDoItem";
 import { ToDoSearch } from "./components/ToDoSearch";
+import { Modal } from "./components/Modal";
+import { NotFound } from "./components/NotFound";
+import { EmptyTasks } from "./components/EmptyTasks";
+import { useTasks } from "./utils/useTasks";
+import { ChangeAlertWithStorageListener } from "./components/ChangeAlert";
 
 function App() {
-  let tasksSaved = JSON.parse(localStorage.getItem("taks"));
-  if (!Array.isArray(tasksSaved)) {
-    tasksSaved = [];
-    localStorage.setItem("taks", JSON.stringify(tasksSaved));
-  }
-
-  const [tasksList, setTasksList] = useState(tasksSaved);
-
+  const [taskToEdit, setTaskToEdit] = useState("");
+  const {
+    tasksList,
+    tasksLeftCounter,
+    tasksDoneCounter,
+    tasksDone,
+    tasksLeft,
+    tasksTotal,
+    taskSearched,
+    searchTasks,
+    setTasksSearched,
+    saveTaks,
+    editTasks,
+    doneTask,
+    deleteTask,
+    openModal,
+    setOpenModal,
+    typeModal,
+    setTypeModal,
+  } = useTasks();
   return (
     <>
-      <ToDoCounter tasksList={tasksList} />
-      <ToDoSearch tasksList={tasksList} setTasksList={setTasksList} />
-      <ToDoList tasksList={tasksList} setTasksList={setTasksList}>
-        {tasksList.map((task) => (
+      <ToDoHeader>
+        <ToDoCounter
+          tasksLeftCounter={tasksLeftCounter}
+          tasksDoneCounter={tasksDoneCounter}
+          tasksDone={tasksDone}
+          tasksLeft={tasksLeft}
+          tasksList={tasksList}
+        />
+        <ToDoSearch
+          setTasksSearched={setTasksSearched}
+          setOpenModal={setOpenModal}
+          setTypeModal={setTypeModal}
+        />
+      </ToDoHeader>
+
+      <ToDoList
+        searchTasks={searchTasks}
+        tasksTotal={tasksTotal}
+        onNotFound={() => <NotFound taskSearched={taskSearched} />}
+        onEmpty={() => <EmptyTasks />}
+        render={(task) => (
           <ToDoItem
+            key={task.id}
             task={task.task}
             taskId={task.id}
             taskDone={task.done}
-            tasksList={tasksList}
-            setTasksList={setTasksList}
-            key={task.id}
+            doneTask={doneTask}
+            deleteTask={deleteTask}
+            setOpenModal={setOpenModal}
+            setTypeModal={setTypeModal}
+            setTaskToEdit={setTaskToEdit}
           />
-        ))}
+        )}
+      >
+        {(task) => (
+          <ToDoItem
+            key={task.id}
+            task={task.task}
+            taskId={task.id}
+            taskDone={task.done}
+            doneTask={doneTask}
+            deleteTask={deleteTask}
+            setOpenModal={setOpenModal}
+            setTypeModal={setTypeModal}
+            setTaskToEdit={setTaskToEdit}
+          />
+        )}
       </ToDoList>
+      {openModal === true && (
+        <Modal
+          setOpenModal={setOpenModal}
+          typeModal={typeModal}
+          saveTaks={saveTaks}
+          taskToEdit={taskToEdit}
+          editTasks={editTasks}
+        />
+      )}
+      <ChangeAlertWithStorageListener />
     </>
   );
 }
--- src/components/ToDoList.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const ToDoList = (props) => {
-  return (
-    <section className="tasksContainer">
-      <ul className="tasksContainer__list">{props.children}</ul>
-    </section>
-  );
-};
